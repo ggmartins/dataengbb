a5a09077f29da4d0bc06ba487192eb0b9f290d76
ggmartins <ggmartins@gmail.com>
2020-07-21 10:23:15 -0500

    fix: moving cert files

--- influxdb/run.sh
+++ influxdb/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "copying $PWD/influxdb.cert.conf $INFLUXDB_PATH/influxdb.cert.conf"
+cp $PWD/influxdb.cert.conf $INFLUXDB_PATH/influxdb.cert.conf
+echo "copying $PWD/cert $INFLUXDB_PATH"
+cp -R $PWD/cert $INFLUXDB_PATH/
+
 docker run --rm \
 	-e INFLUXDB_DB=$INFLUXDB_DB \
 	-e INFLUXDB_HTTP_AUTH_ENABLED=true \
@@ -11,7 +16,7 @@ docker run --rm \
 	-e INFLUXDB_READ_USER_PASSWORD=$INFLUXDB_READ_USER_PASSWORD \
 	-e INFLUXDB_WRITE_USER=$INFLUXDB_WRITE_USER \
 	-e INFLUXDB_WRITE_USER_PASSWORD=$INFLUXDB_WRITE_USER_PASSWORD \
-	-v $PWD:/var/lib/influxdb influxdb /init-influxdb.sh
+	-v $INFLUXDB_PATH:/var/lib/influxdb influxdb /init-influxdb.sh
 
 docker run -p $INFLUXDB_PORT:8086 --name influxdb -d \
-	-v $PWD:/var/lib/influxdb influxdb
+	-v $INFLUXDB_PATH:/var/lib/influxdb influxdb -config /var/lib/influxdb/influxdb.cert.conf
