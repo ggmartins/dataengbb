16fd9f4c16bd52e490566fe5d9f31573f818f8a6
ggmartins <ggmartins@gmail.com>
2020-08-27 17:15:06 -0500

    feat: influxdb grafana worldmap plugin examples

--- influxdb/insert.sh
+++ influxdb/insert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TS=$(echo $(date +%s)'000000000')
-echo "testing insert: "$TS
+echo "testing "$TS
 curl -i -XPOST 'https://'$INFLUXDB_HOST':'$INFLUXDB_PORT'/api/v2/write?bucket='$INFLUXDB_DB'/rp&precision=ns' \
 	--header 'Authorization: Token '$INFLUXDB_WRITE_USER':'$INFLUXDB_WRITE_USER_PASSWORD \
-	--data-raw 'testmetric,tag1=tag1value,tag2=tag2value value=0.123 '$TS
+	--data-raw 'testmetric,tag1=tag1value\,\ tag1value2,tag3=tag3value value=0.123 '$TS
--- influxdb/insert_worldmap.sh
+++ influxdb/insert_worldmap.sh
@@ -4,4 +4,20 @@ TS=$(echo $(date +%s)'000000000')
 echo "testing insert: "$TS
 curl -i -XPOST 'https://'$INFLUXDB_HOST':'$INFLUXDB_PORT'/api/v2/write?bucket='$INFLUXDB_DB'/rp&precision=ns' \
 	--header 'Authorization: Token '$INFLUXDB_WRITE_USER':'$INFLUXDB_WRITE_USER_PASSWORD \
-	--data-raw 'geometric,latitude=41.795,longitude=-87.60,lat=41.795,lon=-87.60,country=US,id=100 metric=100 '$TS
+	--data-raw 'geometric,lat=41.795,lng=-87.60,country=US,ispcity=Verizon\,\ Chicago\,IL metric=1 '$TS
+
+curl -i -XPOST 'https://'$INFLUXDB_HOST':'$INFLUXDB_PORT'/api/v2/write?bucket='$INFLUXDB_DB'/rp&precision=ns' \
+	--header 'Authorization: Token '$INFLUXDB_WRITE_USER':'$INFLUXDB_WRITE_USER_PASSWORD \
+	--data-raw 'geometric,lat=40.345,lng=-74.61,country=US,ispcity=Comcast\,\ Princeton\,NJ metric=2 '$TS
+
+curl -i -XPOST 'https://'$INFLUXDB_HOST':'$INFLUXDB_PORT'/api/v2/write?bucket='$INFLUXDB_DB'/rp&precision=ns' \
+	--header 'Authorization: Token '$INFLUXDB_WRITE_USER':'$INFLUXDB_WRITE_USER_PASSWORD \
+	--data-raw 'geometric,lat=37.368,lng=-122.036,country=US,ispcity=AT&T\,\ Sunnyvale\,CA metric=3 '$TS
+
+curl -i -XPOST 'https://'$INFLUXDB_HOST':'$INFLUXDB_PORT'/api/v2/write?bucket='$INFLUXDB_DB'/rp&precision=ns' \
+	--header 'Authorization: Token '$INFLUXDB_WRITE_USER':'$INFLUXDB_WRITE_USER_PASSWORD \
+	--data-raw 'geometric,lat=47.606,lng=-122.332,country=US,ispcity=Charter\,\ Seattle\,WA metric=0 '$TS
+
+curl -i -XPOST 'https://'$INFLUXDB_HOST':'$INFLUXDB_PORT'/api/v2/write?bucket='$INFLUXDB_DB'/rp&precision=ns' \
+	--header 'Authorization: Token '$INFLUXDB_WRITE_USER':'$INFLUXDB_WRITE_USER_PASSWORD \
+	--data-raw 'geometric,lat=40.712,lng=-74.005,country=US,ispcity=T-Mobile\,\ New\ York\,NY metric=-1 '$TS
