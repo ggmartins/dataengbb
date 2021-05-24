8ad563da0377525340b946b84d8142718d41d867
ggmartins <ggmartins@gmail.com>
2021-05-24 16:29:31 -0500

    serverless bootstrap from template

---
+++ serverless/backend1/install.sh
@@ -0,0 +1,9 @@
+
+sls create --template aws-nodejs-typescript --path iotpro
+npm i aws-jdknpm i winston
+npm install serverless@latest -g
+sls plugin install --name serverless-iam-roles-per-function
+sls plugin install --name serverless-aws-documentation
+sls plugin install --name serverless-reqvalidator-plugin
+npm install typescript-json-schema -g
+npm i bcryptjsnpm -i jsonwebtoken
