0dd8f2e253a5084ac963e7df7d51acccd853b03a
Guilherme Martins <ggmartins@gmail.com>
2021-05-24 16:31:31 -0500

    Update install.sh

--- serverless/backend1/install.sh
+++ serverless/backend1/install.sh
@@ -1,6 +1,7 @@
 
 sls create --template aws-nodejs-typescript --path iotpro
-npm i aws-jdknpm i winston
+npm i aws-jdk
+npm i winston
 npm install serverless@latest -g
 sls plugin install --name serverless-iam-roles-per-function
 sls plugin install --name serverless-aws-documentation
