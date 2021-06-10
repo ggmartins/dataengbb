a6226b875035db83d369551d9ecdbbdd28c921d0
ggmartins <ggmartins@gmail.com>
2021-06-10 03:40:04 -0500

    fix: type + logs

--- datarescue/datarescue_ftp.sh
+++ datarescue/datarescue_ftp.sh
@@ -2,8 +2,8 @@
 
 id=$(cat /etc/salt/minion_id)
 for o in `ls /tmp/nm/nm-exp-active-netrics/upload/pending/default/out/`; do
-curl -T $o ftp://$NMFTPUSER:$NMFTPPASS@$NMSRV:$NMFTPPORT/$id/$o
+curl -T /tmp/nm/nm-exp-active-netrics/upload/pending/default/out/$o ftp://$NMFTPUSER:$NMFTPPASS@$NMSRV:$NMFTPPORT/$id/$o >/tmp/log_datarescure_out.log 2>&1
 done
 for p in `ls /tmp/nm/nm-exp-active-netrics/upload/pending/default/pkl/`; do
-curl -T $p ftp://$NMFTPUSER:$NMFTPPASS@#NMSRV:$NMFTPPORT/$id/$p
+curl -T /tmp/nm/nm-exp-active-netrics/upload/pending/default/pkl/$p ftp://$NMFTPUSER:$NMFTPPASS@$NMSRV:$NMFTPPORT/$id/$p >/tmp/log_datarescure_pkl.log 2>&1
 done
